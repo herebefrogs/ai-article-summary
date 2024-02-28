2174cca5a5a18fd2e54d1c4f80f4fd7a786e76ed
Jerome Lecomte <jerome@herebefrogs.com>
2024-02-28 12:07:42 -0500

    Drop esbuild for an Express proxy between client and OpenAI

---
+++ src/serve.mjs
@@ -0,0 +1,49 @@
+import OpenAI from "openai";
+import express from "express";
+
+const apiKey = "<YOUR KEY HERE>";
+
+const client = new OpenAI({ apiKey });
+
+
+const app = express();
+app.use(express.static("www"));
+app.use(express.json()) // for parsing req.body when mine type is application/json
+
+app.post("/summarize", async (req, res) => {
+  const request = {
+    model: "gpt-3.5-turbo-0125",  // GPT-4 Turbo is pricier than GPT 3.5 Turbo, but and stick closer to the text (quoting it rather than extrapolating meaning) and follows the prompt more accurately.
+    messages: [
+      {"role": "system", "content": "You are a newspaper editor conscious to make text fit within a given word limit."},
+      {"role": "user", "content": `Simplify the text delimited by @@@ in as close to ${req.body.count} words as possible: @@@ ${req.body.text} @@@}`},
+    ],
+    // max_tokens: maxTokens, // will cut off sentences
+    seed: 42
+  }
+
+  const then = Date.now();
+  // via OpenAI's NodeJS client lib
+  const completion = await client.chat.completions.create(request);
+
+  // const response = await fetch("api.openai.com/v1/chat/completions", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //     "Authorization": `Bearer ${apiKey}`,
+  //   },
+  //   body: JSON.stringify(request)
+  // })
+  // const completion = await response.json();
+  const elapsed = (Date.now() - then) / 1000;
+  console.log(`received from OpenAI in ${elapsed}s`, completion);
+
+  res.json(completion);
+});
+
+app.listen(8000, () => console.log("Server running on http://localhost:8000"));
+
+
+
+
+
+
---
+++ www/index.js
@@ -0,0 +1,66 @@
+// messy UI
+const articleEl = document.querySelector('#article');
+const summaryLengthEl = document.querySelector('#summary-length');
+const wordsEl = document.querySelector('#words');
+const maxEl = document.querySelector('#max');
+const summaryEl = document.querySelector('#summary');
+const debugEl = document.querySelector('#debug');
+const refreshEl = document.querySelector('#refresh');
+
+let article_words = 0;
+
+const calculateWordsCount = text => text.replaceAll(/[,;\(\)\.\?\!\\\n"]/g, '').split(' ').length;
+
+const updateSummary = async (articleChanged) => {
+  if (!articleEl.value) {
+    summaryEl.innerText = "Please enter an article to summarize"
+    return
+  };
+
+  if (articleChanged) {
+    // calculate the number of words in the article and update the word count slider appropriately
+    article_words = calculateWordsCount(articleEl.value);
+    maxEl.innerText = article_words;
+    wordsEl.max = article_words;
+    wordsEl.step = Math.ceil(article_words / 25);
+    wordsEl.value = Math.ceil(article_words / 2);
+    updateWordCount(wordsEl.value);
+  }
+  
+  let max_tokens = Math.floor(parseInt(wordsEl.value) * 1000 / 750);
+  debugEl.innerText = JSON.stringify({  max_tokens, article_words });
+
+  refreshEl.style.display = 'block';
+
+   // via API endpoint instead
+  console.log('fetching', articleEl.value, wordsEl.value);
+  const then = performance.now()
+  const response = await fetch("http://localhost:8000/summarize", {
+    method: "POST",
+    headers: { "Content-Type": "application/json"},
+    body: JSON.stringify({
+      text: articleEl.value,
+      count: wordsEl.value
+    })
+  })
+  const elapsed = (performance.now() - then) / 1000;
+  console.log('fetched');
+
+  const completion = await response.json();
+
+  // update UI with summary
+  const summary_words = calculateWordsCount(completion.choices[0].message.content);
+  summaryEl.innerText = completion.choices[0].message.content;
+  debugEl.innerText = JSON.stringify({ finish_reason: completion.choices[0].finish_reason, ...completion.usage, max_tokens, article_words, summary_words, elapsed });
+  refreshEl.style.display = 'none';
+}
+
+const updateWordCount = nbWords => {
+  if (!articleEl.value) return;
+
+  summaryLengthEl.innerText = nbWords + ' words';
+}
+
+wordsEl.oninput = e => updateWordCount(e.target.value);
+wordsEl.onchange = _ => updateSummary(false);
+article.oninput = _ => updateSummary(true);
