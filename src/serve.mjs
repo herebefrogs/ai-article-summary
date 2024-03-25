5c152fbe38a53c5f8382b2843b11d41e8d50a651
Jerome Lecomte <jerome@herebefrogs.com>
2024-03-25 13:28:26 -0400

    Added local model options with Ollama

--- src/serve.mjs
+++ src/serve.mjs
@@ -4,6 +4,8 @@ import express from "express";
 const apiKey = "<YOUR KEY HERE>";
 
 const client = new OpenAI({ apiKey });
+// via Ollama running open source models locally
+// const client = new OpenAI({ apiKey, baseURL: "http://localhost:11434/v1" });
 
 
 const app = express();
@@ -13,6 +15,7 @@ app.use(express.json()) // for parsing req.body when mine type is application/js
 app.post("/summarize", async (req, res) => {
   const request = {
     model: "gpt-3.5-turbo-0125",  // GPT-4 Turbo is pricier than GPT 3.5 Turbo, but and stick closer to the text (quoting it rather than extrapolating meaning) and follows the prompt more accurately.
+//    model: "llama2-uncensored",  // via Ollama running locally
     messages: [
       {"role": "system", "content": "You are a newspaper editor conscious to make text fit within a given word limit."},
       {"role": "user", "content": `Simplify the text delimited by @@@ in as close to ${req.body.count} words as possible: @@@ ${req.body.text} @@@}`},
@@ -25,6 +28,7 @@ app.post("/summarize", async (req, res) => {
   // via OpenAI's NodeJS client lib
   const completion = await client.chat.completions.create(request);
 
+  // via Fetch + OpenAI API
   // const response = await fetch("api.openai.com/v1/chat/completions", {
   //   method: "POST",
   //   headers: {
@@ -41,9 +45,3 @@ app.post("/summarize", async (req, res) => {
 });
 
 app.listen(8000, () => console.log("Server running on http://localhost:8000"));
-
-
-
-
-
-
